a5a138b29c8953d37645a82938b7605ea055cc61
KithM <malachite.akin@gmail.com>
2018-10-22 23:31:52 -0500

    Update DJBot.js

--- DJBot.js
+++ DJBot.js
@@ -1,56 +1,35 @@
 // ==/UserScript==
-MPP.client.on('a', function (msg) {
+MPP.client.on(`a`, function (msg) {
+	if (msg.a == `/djhelp`) send(`DJ.U.Bot commands are: /djhelp, /djboom, and /djchatart`);
+	if (msg.a == `/djchatart`) send(`DJ.U.Bot chat art commands are: /djchatartdog, /djchatartcat, and /djchatartfuck. Please do not use any of these if I do not have crown.`);
+	if (msg.a == `/djchatartcat`) {
+		send(`──────▄▀▄─────▄▀▄`);
+		send(`─────▄█░░▀▀▀▀▀░░█▄`);
+		send(`─▄▄──█░░░░░░░░░░░█──▄▄`);
+		send(`█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█`)
+	};
+	if (msg.a == `/djchatartdog`) {
+		send(`░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░`);
+		send(`░█░░░░░░░░▀▄░░░░░░▄░`);
+		send(`█░░▀░░▀░░░░░▀▄▄░░█░█`);
+		send(`█░▄░█▀░▄░░░░░░░▀▀░░█`);
+		send(`█░░▀▀▀▀░░░░░░░░░░░░█`);
+		send(`█░░░░░░░░░░░░░░░░░░█`);
+		send(`█░░░░░░░░░░░░░░░░░░█`);
+		send(`░█░░▄▄░░▄▄▄▄░░▄▄░░█░`);
+		send(`░█░▄▀█░▄▀░░█░▄▀█░▄▀░`);
+		send(`░░▀░░░▀░░░░░▀░░░▀░░░`)};
+	if (msg.a == `/djchatartfuck`) {
+		send(`▒█▀▀▀ ▒█░▒█ ▒█▀▀█ ▒█░▄▀`);
+		send(`▒█▀▀▀ ▒█░▒█ ▒█░░░ ▒█▀▄░`);
+		send(`▒█░░░ ░▀▄▄▀ ▒█▄▄█ ▒█░▒█`);
+	}
+	if (msg.a == `hi`) {
+		send(`Hello ${msg.p.name}! How are you today? Use /djhelp for a list of commands!`);
+	}
+}
 
-	if (msg.a == '/djhelp') MPP.chat.send ('DJ.U.Bot commands are: /djhelp, /djboom, and /djchatart');
-	if (msg.a == '/djchatart') MPP.chat.send ('DJ.U.Bot chat art commands are: /djchatartdog, /djchatartcat, and /djchatartfuck. Please do not use any of these if I do not have crown.');
-	if (msg.a == '/djchatartcat') {
-	MPP.chat.send('──────▄▀▄─────▄▀▄');
-MPP.chat.send('─────▄█░░▀▀▀▀▀░░█▄');
-MPP.chat.send('─▄▄──█░░░░░░░░░░░█──▄▄');
-MPP.chat.send('█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█')};
-	if (msg.a == '/djchatartdog') {
-	MPP.chat.send ('░▄▀▄▀▀▀▀▄▀▄░░░░░░░░░');
-MPP.chat.send ('░█░░░░░░░░▀▄░░░░░░▄░');
-MPP.chat.send ('█░░▀░░▀░░░░░▀▄▄░░█░█');
-MPP.chat.send ('█░▄░█▀░▄░░░░░░░▀▀░░█');
-MPP.chat.send ('█░░▀▀▀▀░░░░░░░░░░░░█');	
-MPP.chat.send ('█░░░░░░░░░░░░░░░░░░█');
-MPP.chat.send ('░█░░▄▄░░▄▄▄▄░░▄▄░░█░');
-MPP.chat.send ('░█░▄▀█░▄▀░░█░▄▀█░▄▀░');
-MPP.chat.send ('░░▀░░░▀░░░░░▀░░░▀░░░')};	
-	if (msg.a == '/djchatartfuck') {
-	MPP.chat.send ('▒█▀▀▀ ▒█░▒█ ▒█▀▀█ ▒█░▄▀');
-MPP.chat.send ('▒█▀▀▀ ▒█░▒█ ▒█░░░ ▒█▀▄░');
-MPP.chat.send ('▒█░░░ ░▀▄▄▀ ▒█▄▄█ ▒█░▒█')};
-	if (msg.a == 'hi') {
-	MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};
-	if (msg.a == 'Hi') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};
-	if (msg.a == 'Hi.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};
-	if (msg.a == 'hi.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};
-	if (msg.a == 'hello') {
-	MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};
-	if (msg.a == 'Hello') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'Hello.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'hello.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'hey') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'hey.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'Hey') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'Hey.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'hiya') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'Hiya') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'hiya.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};	
-	if (msg.a == 'Hiya.') {
-  MPP.chat.send ('Hello '+msg.p.name+'! How are you today? Use /djhelp for a list of commands!')};
+// Message Functions
+function send(msg){
+	MPP.chat.send(msg);
+}
