9c73d976a028b95f8e06e3c4e640cb80aa7b612d
KithM <malachite.akin@gmail.com>
2018-10-22 23:46:47 -0500

    Update DJBot.js

--- DJBot.js
+++ DJBot.js
@@ -4,12 +4,20 @@ MPP.client.on(`a`, function(msg) {
 	var cmd = msg.a.substring(1,msg.a.length);
     var input = msg.a.substring(cmd.length).trim();
 
+	// Non-commands
+	if (greetings.some(function(v) { return msg.a.toLowerCase().indexOf(v) >= 0; })) {
+		send(`Hello ${msg.p.name}! How are you today? Use /djhelp for a list of commands!`);
+	}
+
+	// Commands
 	if(cmd.startsWith(prefix) == false){
 		return;
 	}
 
 	if (cmd == `djhelp`){
-		send(`DJ.U.Bot commands are: /djhelp, /djboom, and /djchatart`);
+		send(`DJ.U.Bot commands are:
+			/djhelp, /djboom, and /djchatart`
+		);
 	}
 	if (cmd == `djchatart`){
 		send(`DJ.U.Bot chat art commands are:
@@ -40,12 +48,11 @@ MPP.client.on(`a`, function(msg) {
 		send(`▒█▀▀▀ ▒█░▒█ ▒█░░░ ▒█▀▄░`);
 		send(`▒█░░░ ░▀▄▄▀ ▒█▄▄█ ▒█░▒█`);
 	}
-	if (cmd == `hi`) {
-		send(`Hello ${msg.p.name}! How are you today? Use /djhelp for a list of commands!`);
-	}
 });
 
 // Message Functions
 function send(msg){
 	MPP.chat.send(msg);
 }
+
+var greetings = [`hi`,`hey`,`heya`,`hiya`,`hio`,`hello`,`hyello`,`henlo`,`wassup`,`wasup`,`wazzup`,`wazup`,`sup`];
